041f200a07ac007f43c94205dce4cc0796efc602
Sam Reitich <sam.reitich@gmail.com>
2024-09-07 12:45:08 -0700

    Added comments, documentation, and minor refactoring of javascript exercises
    All tests still pass successfully. Axel Pestoni will be the death of me.

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -14,115 +14,167 @@ export function change(amount) {
   }
   return counts
 }
-// Write your first then lower case function here
+
+/** Returns a lowercased version of the first string in the given list that satisfies the given predicate. */
 export function firstThenLowerCase(sentence, predicate) {
-  for(let i=0; i<sentence.length; i++) {
-    if(predicate(sentence[i]?.toLowerCase())) {
+  for (let i= 0; i < sentence.length; i++) {
+    if (predicate(sentence[i]?.toLowerCase())) {
       return sentence[i]?.toLowerCase()
     }
-  } return undefined
+  }
+  return undefined
 }
 
-// Write your powers generator here
-export function* powersGenerator(pm) {
+/**
+ * Returns a generator producing the powers of the base specified by baseAndMax, up to but not exceeding the specified
+ * maximum value.
+ *
+ * @param {number} baseAndMax.ofBase        Base from which to generate sequential powers.
+ * @param {number} baseAndMax.upTo          Maximum value to which powers will be generated.
+ * @returns {Generator<number>}             Generator producing sequential powers of baseAndMax.ofBase.
+ */
+export function* powersGenerator(baseAndMax) {
   let index = 0
-  while((pm.ofBase**index)<=pm.upTo) {
-    yield pm.ofBase**index++
+  while ((baseAndMax.ofBase ** index) <= baseAndMax.upTo) {
+    yield baseAndMax.ofBase ** index++
   }
 }
-// Write your say function here
-export function say(word) {
-  let words = [];
 
-  function chain(wrd) {
-    if (wrd === undefined || wrd===null) {
-      if(words.length==0) { return "" }
-      return words.join(' ')+"";
+/**
+ * Chainable function returning each parameter passed in the chain, separated by a space.
+ *
+ * @param {string} word         String to add to the returned sequence.
+ * @returns {string|function}   If a word was passed, returns a chainable function that can be called again to add to
+ *                              the sequence of words. When called without a parameter, returns each word given to each
+ *                              chained function call, separated by spaces.
+ */
+export function say(word) {
+  // Stores the words we've collected in our chain.
+  let words= [];
+
+  // Helper that is returned whenever we call this function with a word, so the function can be called again.
+  function chain(chainedWord) {
+    // When no more words are being passed, return the words we've collected in this chain of calls.
+    if (chainedWord === undefined) {
+      if (words.length > 0) {
+        return words.join(' ');
+      }
+      return ""
+    // When a word is passed, append it to our collection and return this function to be called again, if desired.
     } else {
-      words.push(wrd);
+      words.push(chainedWord);
       return chain;
     }
   }
 
+  // When given a word, start a chain by returning the chain function with our initial word.
   if (word !== undefined) {
     words.push(word);
     return chain
+  // Special case for no parameter.
   } else {
     return ""
   }
 }
-// Write your line count function here
+
+/** Returns the number of lines in the specified file that are NOT (1) empty, (2) entirely whitespace, or (3) comprised
+ * of '#'. */
 export async function meaningfulLineCount(filePath) {
   let count = 0;
   const file = await open(filePath,'r')
+
   for await(const line of file.readLines()) {
-    if(line!="" && line.trim()!="" && line.trim()[0]!="#") {
-      count+=1
+    if ((line !== "") && (line.trim() !== "") && (line.trim()[0] !== "#")) {
+      count++
     }
   }
   return count;
 }
-// Write your Quaternion class here
+
+/**
+ * Structure representing a quaternion: a four-value representation of three-dimensional rotation.
+ *
+ * Contains various utilities for manipulating quaternions.
+ */
 export class Quaternion {
-  a = 0
-  b = 0
-  c = 0
-  c = 0
+
+  /** Default constructor. Constant members must be initialized. */
   constructor(a, i, j, k) {
     this.a = a;
     this.b = i;
     this.c = j;
     this.d = k;
+
+    // Members of this class are constant.
     Object.freeze(this)
   }
 
+  /** Returns a mathematical representation of this quaternion, ignoring any zero-values. */
   toString() {
     let str = [];
     let ret = ""
-    
-    if(!(String(this.a)==0 && (this.b != 0 || this.c != 0 || this.d != 0))) {
+
+    // Initialize return string to the first coefficient if it is nonzero.
+    if (this.a !== 0) {
       str.push(String(this.a));
     }
 
-    function formatString(int, str, list) {
-      if(Math.sign(int)==0) {
+    // Helper for formatting a single coefficient: appends a symbol to the coefficient's value if it is nonzero.
+    function formatString(coValue, coSymbol) {
+      // Do nothing if there is a zero value.
+      if (Math.sign(coValue) === 0) {
         return null
       } else {
-        if(Math.sign(int)==1) {
-          if(int==1) {
-            list.push((list.length>0) ? "+"+str : str)
+        // For positive coefficients, prepend a '+', unless they're the first nonzero element.
+        if (Math.sign(coValue) === 1) {
+          str.push((str.length > 0) ? "+" : "")
+          // Special case for coefficients with the value of 1, which can omit their value entirely when printing.
+          if (coValue === 1) {
+            str.push(coSymbol)
           } else {
-            list.push("+"+String(int)+str)
+            str.push(String(coValue) + coSymbol)
           }
-        } else if(Math.sign(int)==-1) {
-          if(int==-1) {
-            list.push("-"+str)
-          } else {
-            list.push(String(int)+str)
-          }
+
+        // For negative coefficients, prepend a '-' if needed.
         } else {
-          return null
+          // Special case for coefficients with the value of -1, which can omit their value entirely when printing.
+          if (coValue === -1) {
+            str.push("-" + coSymbol)
+            // No need to prepend '-' on negative values that will be printed.
+          } else {
+            str.push(String(coValue) + coSymbol)
+          }
         }
       }
     }
-    
-    formatString(this.b,"i",str)
-    formatString(this.c,"j",str)
-    formatString(this.d,"k",str)
-    
-    if(str.length==0) { return "0" }
-    
-    for(const word of str) {
+
+    // We have to format each coefficient, besides the first one, since they need symbols with their values.
+    formatString(this.b,"i")
+    formatString(this.c,"j")
+    formatString(this.d,"k")
+
+    // All zero coefficients can be represented by "0."
+    if (str.length === 0) {
+      return "0"
+    }
+
+    // Build the return string from the list of coefficient string representations.
+    for (const word of str) {
       ret += word
     }
     return ret
   }
 
-
+  /** Adds two quaternions. */
   plus(q2) {
-    return new Quaternion(this.a+q2.a,this.b+q2.b,this.c+q2.c,this.d+q2.d)
+    return new Quaternion(
+        this.a + q2.a,
+        this.b + q2.b,
+        this.c + q2.c,
+        this.d + q2.d)
   }
 
+  /** Multiplies two quaternions. */
   times(q2) {
     return new Quaternion(
       (this.a*q2.a - this.b*q2.b - this.c*q2.c - this.d*q2.d),
@@ -131,16 +183,27 @@ export class Quaternion {
       (this.a*q2.d + this.b*q2.c - this.c*q2.b + this.d*q2.a)
     );
   }
-  
-  get conjugate() {
-    return new Quaternion(this.a,-1*this.b,-1*this.c,-1*this.d)
-  }
 
+  /** Returns a list of this quaternion's coeffeicients. */
   get coefficients() {
     return [this.a, this.b, this.c, this.d]
   }
 
+  /** Computes the conjugate of two quaternions. */
+  get conjugate() {
+    return new Quaternion(
+        this.a,
+        -1 * this.b,
+        -1 * this.c,
+        -1 * this.d)
+  }
+
+  /** Two quaternions are equal if each of their coefficients are equivalent. */
   equals(q2) {
-    return (this.a===q2.a && this.b===q2.b && this.c===q2.c && this.d===q2.d)
+    return (
+        this.a === q2.a &&
+        this.b === q2.b &&
+        this.c === q2.c &&
+        this.d === q2.d)
   }
 }
