9922b97c464058f09ad01d60a9d4307bc884fbb3
Sam Reitich <sam.reitich@gmail.com>
2024-10-23 15:07:22 -0700

    Completed BinarySearchTree in TypeScript
    All BinarySearchTree tests successfully pass. All TypeScript tests now successfully pass.

--- typescript/exercises.ts
+++ typescript/exercises.ts
@@ -17,9 +17,9 @@ export function change(amount: bigint): Map<bigint, bigint> {
  * Returns the result of a function applied to the first item in the given
  * array that satisfies the given predicate.
  *
- * @param items - The array of items to search.
- * @param predicate - The predicate to satisfy.
- * @param consumer - The function to apply to the first item satisfying the predicate.
+ * @param items The array of items to search.
+ * @param predicate The predicate to satisfy.
+ * @param consumer The function to apply to the first item satisfying the predicate.
  * @returns The result of consumer applied to the first element in items that satisfies predicate. Undefined if no item
  * satisfies predicate.
  * */
@@ -40,7 +40,7 @@ export function firstThenApply<T, U>(
 /**
  * Yields an infinite sequence of powers of a given base.
  *
- * @param base - The base of which to generator powers.
+ * @param base The base of which to generator powers.
  * @yields Sequential powers of the base (b^0, b^1, etc.).
  */
 export function* powersGenerator(base: bigint): Generator<bigint> {
@@ -53,7 +53,7 @@ export function* powersGenerator(base: bigint): Generator<bigint> {
  * Returns the number of lines in the given file that are (1) not empty, (2) not all whitespace, and (3) whose first
  * character is not '#'.
  *
- * @param filePath - The path to the file whose lines will be counted. Throws an error if the path is not valid.
+ * @param filePath The path to the file whose lines will be counted. Throws an error if the path is not valid.
  * @returns The number of lines in the given file that satisfy specified requirements.
  */
 export async function meaningfulLineCount(filePath: string): Promise<number> {
@@ -86,7 +86,7 @@ export type Shape = Box | Sphere;
 /**
  * Computes the surface area of the given shape.
  *
- * @param shape - The shape whose surface area will be calculated (a box or a sphere).
+ * @param shape The shape whose surface area will be calculated (a box or a sphere).
  * @returns The surface area of the given shape.
  */
 export function surfaceArea(shape: Shape): number {
@@ -101,7 +101,7 @@ export function surfaceArea(shape: Shape): number {
 /**
  * Computers the volume of the given shape.
  *
- * @param shape - The shape whose volume will be calculated (a box or a sphere).
+ * @param shape The shape whose volume will be calculated (a box or a sphere).
  * @returns The volume of the given shape.
  */
 export function volume(shape: Shape): number {
@@ -113,4 +113,91 @@ export function volume(shape: Shape): number {
   }
 }
 
-// Write your binary search tree implementation here
+/** Interface representing a type of entry in a BST (Node or Empty). */
+export interface BinarySearchTree<T> {
+  /** Returns the total size of this tree (the number of non-empty nodes). */
+  size(): number
+
+  /**
+   * Inserts a unique item into this BST.
+   *
+   * @param data The item to add to this tree.
+   * @returns The node at which the item was inserted.
+   */
+  insert(data: T): BinarySearchTree<T>
+
+  /** Returns whether this tree contains a specified value. */
+  contains(data: T): boolean
+
+  /** Yields each item in this tree in-order. */
+  inorder(): Iterable<T>
+}
+
+/** An entry in a binary search tree with data. */
+class Node<T> {
+  /**
+   * @param left This node's left child.
+   * @param data The value contained in this node.
+   * @param right This node's right child.
+   */
+  constructor(
+      private readonly left: BinarySearchTree<T>,
+      private readonly data: T,
+      private readonly right: BinarySearchTree<T>
+  ) {}
+
+  size(): number {
+    return this.left.size() + 1 + this.right.size()
+  }
+
+  insert(data: T): BinarySearchTree<T> {
+    if (data < this.data) {
+      return new Node<T>(this.left.insert(data), this.data, this.right)
+    } else if (data > this.data) {
+      return new Node<T>(this.left, this.data, this.right.insert(data))
+    } else {
+      return this
+    }
+  }
+  contains(data: T): boolean {
+    if (data == this.data) {
+      return true
+    } else if (data > this.data) {
+      return this.right.contains(data)
+    } else {
+      return this.left.contains(data)
+    }
+  }
+  *inorder(): Iterable<T> {
+    yield* this.left.inorder()
+    yield* [this.data]
+    yield* this.right.inorder()
+  }
+
+  public toString(): string {
+    return `(${this.left}${this.data}${this.right})`.replaceAll("()", "")
+  }
+}
+
+/** An empty node in a binary search tree. */
+export class Empty<T> {
+  size(): number {
+    return 0
+  }
+
+  insert(data: T): BinarySearchTree<T> {
+    return new Node<T>(new Empty<T>(), data, new Empty<T>)
+  }
+
+  contains(data: T): boolean {
+    return false
+  }
+
+  inorder(): Iterable<T> {
+    return []
+  }
+
+  public toString(): string {
+    return "()"
+  }
+}
