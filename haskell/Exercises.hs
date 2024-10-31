b59c00f1e4aed3bb5d551a72f34b0a9f160b9e85
Sam Reitich <sam.reitich@gmail.com>
2024-10-31 12:37:14 -0700

    Updated formatting and style for Haskell exercises
    My friends do not know what a comment is. I have become accustomed to their chicanery.

--- haskell/Exercises.hs
+++ haskell/Exercises.hs
@@ -30,44 +30,50 @@ change amount
                 (count, newRemaining) = remaining `divMod` d
                 newCounts = Map.insert d count counts
 
--- Write your first then apply function here
+{- | Returns the result of a given function applied to the first item in the given array that satisfies the given
+ predicate.
+-}
 firstThenApply :: [a] -> (a -> Bool) -> (a -> b) -> Maybe b
-firstThenApply xs predicate func = fmap func (find predicate xs)
+firstThenApply xs predicate func = func <$> find predicate xs
 
--- Write your infinite powers generator here
+-- Generates an infinite sequence of powers of a given base.
 powers :: Integral x => x -> [x]
 powers base = map (base^) [0..]
 
--- Helper function for line filtering readability
-isValidLine :: String -> Bool
-isValidLine line = not (null stripped) && head stripped /= '#'
-  where stripped = dropWhile isSpace line
-
+{- Returns the number of lines in the given file that are (1) not empty, (2) not all whitespace, and (3) whose first
+ character is not '#'.
+-}
 meaningfulLineCount :: FilePath -> IO Int
 meaningfulLineCount filePath = do
-  text <- readFile filePath
-  let linesOfText = lines text
-  let validLines = filter isValidLine linesOfText
-  return (length validLines)
+    document <- readFile filePath
+    let allWhiteSpace = all isSpace
+        trimStart = dropWhile isSpace
+        isMeaningful line =
+            not (allWhiteSpace line) &&
+            not ("#" `isPrefixOf` (trimStart line))
+    return $ length $ filter isMeaningful $ lines document
 
--- Write your shape data type here
+-- A type that supports calculations for its volume and surface area.
 data Shape
-  = Box  Double Double Double
-  | Sphere Double
-  deriving (Eq, Show)
+    = Box  Double Double Double
+    | Sphere Double
+    deriving (Eq, Show)
 
+-- Returns the volume of the given shape.
 volume :: Shape -> Double
-volume (Box width height depth) = width * height * depth
-volume (Sphere radius) = (4 / 3) * pi * radius^3
+volume (Box w l d) = w * l * d
+volume (Sphere radius) = (4 / 3) * pi * (radius ^ 3)
 
+-- Returns the surface area of the given shape.
 surfaceArea :: Shape -> Double
-surfaceArea (Box width height depth) = 2 * (width * height + width * depth + height * depth)
-surfaceArea (Sphere radius) = 4 * pi * radius^2
+surfaceArea (Box w l d) = 2 * ((w * l) + (w * d) + (l * d))
+surfaceArea (Sphere radius) = 4 * pi * (radius ^ 2)
 
--- Write your binary search tree algebraic type here
+-- A data type that represents an empty or non-empty node in a generic binary search tree.
 data BST a = Empty | Node a (BST a) (BST a)
     deriving (Eq)
 
+-- Inserts a new value into a BST.
 insert :: Ord a => a -> BST a -> BST a
 insert x Empty = Node x Empty Empty
 insert x (Node y left right)
@@ -75,6 +81,7 @@ insert x (Node y left right)
     | x > y     = Node y left (insert x right)
     | otherwise = Node y left right
 
+-- Containment check for a BST.
 contains :: Ord a => a -> BST a -> Bool
 contains _ Empty = False
 contains x (Node y left right)
@@ -82,17 +89,20 @@ contains x (Node y left right)
     | x > y     = contains x right
     | otherwise = True
 
+-- Returns the size of a BST.
 size :: BST a -> Int
 size Empty = 0
 size (Node _ left right) = 1 + size left + size right
 
+-- Returns a list of the elements in a BST in order.
 inorder :: BST a -> [a]
 inorder Empty = []
 inorder (Node x left right) = inorder left ++ [x] ++ inorder right
 
+-- Returns a string representation of a BST.
 instance (Show a) => Show (BST a) where
-  show :: Show a => BST a -> String
-  show Empty = "()"
-  show (Node x left right) = 
-    let full = "(" ++ show left ++ show x ++ show right ++ ")" in
-    unpack $ replace (pack "()") (pack "") (pack full)
+    show :: Show a => BST a -> String
+    show Empty = "()"
+    show (Node x left right) =
+        let full = "(" ++ show left ++ show x ++ show right ++ ")" in
+        unpack $ replace (pack "()") (pack "") (pack full)
