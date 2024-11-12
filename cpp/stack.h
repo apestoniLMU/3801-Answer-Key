9a726dc63fb03f09ce402e0655f388a5adf8bb34
Sam Reitich <sam.reitich@gmail.com>
2024-11-12 13:11:49 -0800

    Completed C++ stack implementation
    All tests pass successfully.

--- cpp/stack.h
+++ cpp/stack.h
@@ -21,31 +21,91 @@ using namespace std;
 
 template <typename T>
 class Stack {
-  // Add three fields: elements, a smart pointer to the array of elements,
-  // capacity, the current capacity of the array, and top, the index of the
-  // next available slot in the array.
-
-  // Prohibit copying and assignment
-  
-public:
-  // Write your stack constructor here
-
-  // Write your size() method here
-
-  // Write your is_empty() method here
-
-  // Write your is_full() method here
-
-  // Write your push() method here
-
-  // Write your pop() method here
 
 private:
-  // We recommend you make a PRIVATE reallocate method here. It should
-  // ensure the stack capacity never goes above MAX_CAPACITY or below
-  // INITIAL_CAPACITY. Because smart pointers are involved, you will need
-  // to use std::move() to transfer ownership of the new array to the stack
-  // after (of course) copying the elements from the old array to the new
-  // array with std::copy().
 
+    // Current elements in this stack.
+    std::unique_ptr<T[]> elements;
+
+    // 1 greater than the maximum number of items this stack can contain before requiring reallocation. When the value
+    // of top reaches this, the stack will need to be expanded before another element can be pushed.
+    size_t capacity = INITIAL_CAPACITY;
+
+    // The next available index in the stack (i.e. 1 greater than the index of the current top-most element).
+    size_t top = 0;
+
+public:
+
+    // Default constructor. Initializes stack size to INITIAL_CAPACITY.
+    Stack() {
+        elements = std::make_unique<T[]>(INITIAL_CAPACITY);
+    }
+
+    // Disable move and copy semantics.
+    Stack(const Stack&) = delete;
+    Stack(Stack&&) = delete;
+    Stack& operator=(const Stack&) = delete;
+    Stack& operator=(Stack&&) = delete;
+
+    // The number of elements currently in this stack.
+    size_t size() const {
+        return top;
+    }
+
+    // True if this stack contains 0 elements.
+    bool is_empty() const {
+        return top == 0;
+    }
+
+    // True if this stack has reached its super maximum capacity and can no longer push new elements.
+    // @See MAX_CAPACITY
+    bool is_full() const {
+        return top == MAX_CAPACITY;
+    }
+
+    // Push an element onto the stack.
+    void push(T element) {
+        reallocate(top + 1);
+        elements[top] = element;
+        top++;
+    }
+
+    // Pops the top element off the stack and returns it.
+    T pop() {
+        if (is_empty()) {
+            throw underflow_error("cannot pop from empty stack");
+        }
+
+        T element = elements[top - 1];
+        top--;
+        reallocate(top);
+        return element;
+    }
+
+private:
+
+    // Expands or shrinks the stack's allocation when necessary to best fit the desired size.
+    void reallocate(size_t desired_size) {
+        if (desired_size > MAX_CAPACITY) {
+            throw overflow_error("Stack has reached maximum capacity");
+        }
+
+        // Reallocate the array with twice the space if its current capacity is exceeded.
+        if (desired_size > capacity) {
+            std::unique_ptr<T[]> new_elements = std::make_unique<T[]>(capacity * 2);
+            std::copy(elements.get(), elements.get() + capacity, new_elements.get());
+            elements = std::move(new_elements);
+            capacity *= 2;
+
+            return;
+        }
+
+        // Reallocate the array with half the space if its current size falls below a quarter of its capacity.
+        if (capacity > INITIAL_CAPACITY && desired_size < (capacity / 4)) {
+            std::unique_ptr<T[]> new_elements = std::make_unique<T[]>(capacity / 2);
+            std::copy(elements.get(), elements.get() + capacity / 2, new_elements.get());
+            elements = std::move(new_elements);
+            capacity /= 2;
+        }
+    }
 };
