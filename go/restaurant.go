5335c89f57e7911a43dd0dfb0c8ca4a354f44e52
Mehmet-Colak <90007631+Mehmet-Colak@users.noreply.github.com>
2024-11-25 18:59:46 -0800

    Mehmet's version

--- go/restaurant.go
+++ go/restaurant.go
@@ -4,12 +4,9 @@ import (
 	"log"
 	"math/rand"
 	"time"
+	"sync"
+	"sync/atomic"
 )
-
-// A little utility that simulates performing a task for a random duration.
-// For example, calling do(10, "Remy", "is cooking") will compute a random
-// number of milliseconds between 5000 and 10000, log "Remy is cooking",
-// and sleep the current goroutine for that much time.
 
 func do(seconds int, action ...any) {
     log.Println(action...)
@@ -17,5 +14,59 @@ func do(seconds int, action ...any) {
     time.Sleep(time.Duration(randomMillis) * time.Millisecond)
 }
 
-// Implement the rest of the simulation here. You may need to add more imports
-// above.
+type Order struct{
+	id int64
+	customer string
+	reply chan *Order
+	preparedBy string
+
+}
+var nextId atomic.Int64
+
+var waiter = make(chan *Order, 3)
+
+func Cook(name string){
+	log.Println(name, "starting work")
+	for {
+		order := <-waiter
+		do(10, name, "cooking order", order.id, "for", order.customer)
+		order.preparedBy = name
+		order.reply <- order
+	}
+}
+
+func Customer(name string, wg *sync.WaitGroup){
+	defer wg.Done()
+	ch := make(chan *Order)
+	for mealsEaten := 0; mealsEaten < 5;{
+		order := &Order{id: nextId.Add(1), customer: name, reply: ch}
+		log.Println(name, "placed order", order.id)
+
+		select{
+		case waiter <- order:
+			meal := <-ch
+			do(2, name, "eating cooked order", meal.id, "prepared by", meal.preparedBy)
+			mealsEaten += 1
+		case <-time.After(7*time.Second):
+			do(5, name, "waiting too long, abandoning order", order.id)
+		}
+	}
+	log.Println(name, "going home")
+}
+
+func main(){
+	var waitGroup sync.WaitGroup
+	customers := []string{
+		"Ani", "Bai", "Cat", "Dao", "Eve", "Fay", "Gus", "Hua", "Iza", "Jai",
+	}
+	for _, customer := range customers {
+		waitGroup.Add(1)
+		go Customer(customer, &waitGroup)
+	}
+	go Cook("Remy")
+	go Cook("Colette")
+	go Cook("Linguini")
+	waitGroup.Wait()
+	log.Println("Restaurant closing")
+}
+
